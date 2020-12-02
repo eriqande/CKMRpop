f235b4d0275d90d7ad37a7ed887caeab271684e9
Eric C. Anderson <eric.anderson@noaa.gov>
2020-12-02 12:10:06 -0700

    Added species 2 life history
    At least I started this.  It will need some tweaking once I start looking at the consequences in the simulations...

--- R/data.R
+++ R/data.R
@@ -7,3 +7,15 @@
 #' @docType data
 #' @name species_1_life_history
 NULL
+
+
+
+#' a list of life-history / life-table data for another hypothetical species
+#'
+#' species_2 for examples.  Note that I just set the male and female
+#' rates and parameters similar.  Will probably need to adjust this
+#' once I start playing with it.
+#' @source Fill this in later
+#' @docType data
+#' @name species_2_life_history
+NULL
