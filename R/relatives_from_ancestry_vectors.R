3e650a4389aba72c45fabd8b744e2e1e2f1ac23b
Eric C. Anderson <eric.anderson@noaa.gov>
2021-08-31 14:58:05 -0600

    Clean up some CRAN check notes and add further documentation.

--- R/relatives_from_ancestry_vectors.R
+++ R/relatives_from_ancestry_vectors.R
@@ -9,9 +9,14 @@
 #' one ID within their ancestry vectors.  The way this function
 #' works is by, for each ID, making a vector of all the other IDs
 #' that include that original ID in its ancestry vectors.  Then,
-#' for each individuals, these vectors are catenated (one for each
-#' anestor) then they are uniqued and sorted.
-#' @param A a tibble that has columns of `ID` and `ancestors`.
+#' for each individual, these vectors are catenated (one for each
+#' ancestor) then values are made unique and sorted, and returned in
+#' a list column of relatives.
+#' @param A a tibble that has columns of `ID` and `ancestors`. `ID` is the ID of the
+#' individual and `ancestors` is a list column that holds ancestry vectors.
+#' @return This function returns the original input, `A`, with an additional
+#' list column called `relatives`, that includes vectors of the samples that
+#' each sample is related to.
 #' @export
 relatives_from_ancestry_vectors <- function(A) {
   tmp <- A %>%
